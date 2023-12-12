87be5e11b260f31dd9448ada23d4479f03493c79
zilong10 <ajaysrchaudhary@gmail.com>
2023-12-12 21:09:01 +0545

    new work added

--- src/components/workpage/WorkpageData.js
+++ src/components/workpage/WorkpageData.js
@@ -1,6 +1,7 @@
 import Aron from "../../assets/aron.png";
 import Ilami from "../../assets/ilamiagro.jpg";
 import Visional from "../../assets/visional.jpg";
+import Ptatts from "../../assets/ptatts.png";
 
 export const WorkData = [
   {
@@ -28,6 +29,15 @@ export const WorkData = [
     img: Visional,
     title: "VISIONAL - A live shopping platform",
     task: "Design & Development",
+    date: "2023",
+  },
+  {
+    id: 3,
+    link: "https://constructible-chapl.000webhostapp.com/",
+    category: "Development",
+    img: Ptatts,
+    title: "PTATTS - A tattoo art and designing studio",
+    task: "Development",
     date: "2023",
   },
 ];
--- src/components/worksection/WorkData.js
+++ src/components/worksection/WorkData.js
@@ -1,6 +1,7 @@
 import Aron from "../../assets/aron.png";
 import Ilami from "../../assets/ilamiagro.jpg";
 import Visional from "../../assets/visional.jpg";
+import Ptatts from "../../assets/ptatts.png";
 
 export const Data = [
   {
@@ -28,6 +29,16 @@ export const Data = [
     title: "VISIONAL",
     para: "A live shopping platform",
     task: "Design & Development",
+    date: "2023",
+  },
+
+  {
+    worknum: 4,
+    link: "https://constructible-chapl.000webhostapp.com/",
+    img: Ptatts,
+    title: "PTATTS",
+    para: "A tattoo art and designing studio",
+    task: "Development",
     date: "2023",
   },
 ];
